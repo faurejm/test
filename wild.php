a545c5917d60b8d0dfe71eb3f58e8b88bc7c8999
faurejm <faure.jm@gmail.com>
2018-10-11 15:42:58 +0200

    test message

---
+++ wild.php
@@ -0,0 +1,20 @@
+<?php 
+
+$MovieName = 'Indiana Jones and the Last Crusade';
+$MovieSeen = true;
+$MovieYear = 1989;
+$MovieNote= 8.3;
+
+$ConvBool = ($MovieSeen) ? 'Yes' : 'No';
+
+echo "Movie: ";
+echo $MovieName;
+echo "<br>";
+echo "Seen: ";
+echo $ConvBool;
+echo "<br>";
+echo "Release Date: ";
+echo $MovieYear;
+echo "<br>";
+echo "Note: ";
+echo $MovieNote;
